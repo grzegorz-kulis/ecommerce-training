16adfcd9ac36560072fc26257ecdb8e0af364671
lisek <kulisgrze@gmail.com>
2021-10-30 19:12:43 +0200

    - liqiuibase migration setup - exposed basic REST endpoints - introduced Cross Origin, localhost based for now - introduced PostgreSQL database, localhost based for now

--- src/main/java/com/lisek/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/lisek/ecommerce/dao/ProductRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
+// TODO: should end up in properties
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
